a744fbb4b508de9fb6621e375c97db67903f0c3d
sultangorgulu <sugorgulupriv@gmail.com>
2023-07-04 13:13:38 +0300

    Sprint2
    Tasarımı yeniden yaptım. Tasarım kodlarını yazdım. Firebase bağlantısını yaptım. Login ve Sign up sayfalarını da ekledim. Üye olunduğunda aktif bir şekilde firebase bilgiler düşüyor.

---
+++ android/app/src/main/kotlin/com/example/flutter_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sportif1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/customColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static const scaffoldBackgroundColor = Color(0xFFFFD1DF);
+  static const textButtonColor = Color(0xFF1FA774);
+  static const loginButtonTextColor = Color(0xFF1FA774);
+}
